c408a39b4da7bbafd24c23ea1c434a08580acf5a
Husna Kider <hkider@email.unc.edu>
2021-05-20 11:53:24 -0400

    Exercise 01: hype_machine finished!

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,10 @@
-"""DESCRIPTION OF PROGRAM HERE"""
+"""Using inputs, concatenation to hype myself up with inspirational messages."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730401081"
 
 # Your solution starts here...
+name: str =  input("What is your name? ")
+
+print(name + ", you have overcome 100% of the days you've faced!")
+print("I am proud of you, " + name + ".")
+print("Resting yourself, dear " + name + ", is investing in yourself.")
